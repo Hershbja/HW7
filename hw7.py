2dd5bec9be0e60088dfab23ceb2df953f2662d76
Ander <ander.the.enderwolf@gmail.com>
2021-03-04 10:27:59 -0800

    Wrote code to pass 400 to return true

--- hw7.py
+++ hw7.py
@@ -24,7 +24,12 @@ def FizzBuzz(test):
 def leapyear(year):
     if year % 4 == 0:
         if year % 100 == 0:
-            return False
+            if year % 400 == 0:
+                return True
+            else:
+                return False
         else:
             return True
+    else:
+        return False
         
