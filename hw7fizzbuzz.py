55f84ea65f57dcd35290627351b1bf40ef2991aa
Ander <ander.the.enderwolf@gmail.com>
2021-03-04 10:32:24 -0800

    Seperated the files because i'm a dolt

--- hw7fizzbuzz.py
+++ hw7fizzbuzz.py
@@ -20,16 +20,3 @@ def FizzBuzz(test):
                 return x
             else:
                 print(x)
-            
-def leapyear(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
-        
